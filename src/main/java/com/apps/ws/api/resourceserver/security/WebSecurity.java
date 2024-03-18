06df11930bd1e613878c60c796d0f05240798906
Inna Lapina <jyliana@gmail.com>
2024-03-18 19:02:31 +0200

    adding resource server

---
+++ src/main/java/com/apps/ws/api/resourceserver/security/WebSecurity.java
@@ -0,0 +1,37 @@
+package com.apps.ws.api.resourceserver.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity(securedEnabled=true)
+@EnableWebSecurity
+public class WebSecurity {
+
+  @Bean
+  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+    JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
+
+    http
+            .authorizeHttpRequests((auth) -> auth.requestMatchers(HttpMethod.GET, "/users/status/check")
+                    // .hasAuthority("SCOPE_profile")
+                    .hasRole("developer")
+                    // .hasAnyAuthority("ROLE_developer")
+                    // .hasAnyRole("developer","user")
+                    .anyRequest().authenticated())
+            .oauth2ResourceServer(
+                    oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+
+    return http.build();
+  }
+
+}
